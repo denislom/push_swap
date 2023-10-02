692e64a8e6f58454bb1f4e2eaa6c8f24b40ed902
Denis Lom <lom.denis@seznam.cz>
2023-10-02 23:24:40 +0200

    010101

--- push_swap.c
+++ push_swap.c
@@ -1,12 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 00:42:04 by dlom              #+#    #+#             */
-/*   Updated: 2023/09/30 00:42:05 by dlom             ###   ########.fr       */
+/*   Updated: 2023/10/02 23:24:17 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a; // stack
+
+	a = ft_process(argc, argv);
+	if (!a || ft_checkdup(a)) //check if there are duplicates
+	{
+		ft_free(&a);
+		ft_error();
+	}
+	if (!ft_checksorted(a)) // check if the stack is sorted
+		ft_sort(&a); //if not sorted, let's sort
+	ft_free(&a);
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 23:18:39 by dlom              #+#    #+#             */
+/*   Updated: 2023/10/02 23:19:43 by dlom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+// # include "../libft/libft.h"
+
+typedef struct s_stack
+{
+	long			nbr;
+	long			index;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
+
+#endif

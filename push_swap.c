c1c07ac8b8cc0e88f28afafb65817658f2b24759
Denis Lom <lom.denis@seznam.cz>
2023-11-19 00:28:43 +0100

    link for generator of random numbers

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 00:42:04 by dlom              #+#    #+#             */
-/*   Updated: 2023/11/18 21:51:13 by dlom             ###   ########.fr       */
+/*   Updated: 2023/11/19 00:27:12 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ PLfqABt5AS4FmXeWuuNDS3XGENJO1VYGxl
 	- Code Vault: Linked lists
 Checker_linux:
 https://github.com/rlinsdev/42-Push-swap
+Random generator:
+https://numbergenerator.org/randomnumbergenerator
 
 	// JUST TEST OF THE LIBFT
 	// ft_printf("ahoj");
